0f8a29420adc47190ce814a727173cebd47ef936
McNoHam14 <McNoHam14@gmail.com>
2023-03-04 12:06:50 +0000

    put complete

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -17,7 +17,7 @@ console.log(
   join(dirname(fileURLToPath(import.meta.url)), "authors.json")
 );
 
-// POST
+// POST (new author)
 
 authorsRouter.post("/", (req, res) => {
   res.send({ message: "Hello I am the POST endpoint" });
@@ -29,39 +29,63 @@ authorsRouter.post("/", (req, res) => {
     id: uniqid(),
   };
 
-  const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
 
-  authors.push(newAuthor);
+  authorsArray.push(newAuthor);
 
   console.log("B", newAuthor);
 
-  fs.writeFileSync(authorsJSONPath, JSON.stringify(authors));
+  fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray));
 
   res.status(201).send({ id: newAuthor.id });
 });
+
+// GET (all authors)
 
 authorsRouter.get("/", (req, res) => {
   const fileContentAsBuffer = fs.readFileSync(authorsJSONPath);
   console.log("C", fileContentAsBuffer);
 
   console.log("D", JSON.parse(fileContentAsBuffer));
-  const authors = JSON.parse(fileContentAsBuffer);
+  const authorsArray = JSON.parse(fileContentAsBuffer);
 
-  res.send(authors);
+  res.send(authorsArray);
 });
+
+// GET (single author)
 
 authorsRouter.get("/:authorId", (req, res) => {
   //   res.send({ message: "Single author endpoint " });
 
   //   console.log("ID:", req.params.userId);
 
-  const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
 
-  const author = authors.find((author) => author.id === req.params.authorId);
+  const author = authorsArray.find(
+    (author) => author.id === req.params.authorId
+  );
 
   console.log(author);
 
   res.send(author);
+});
+
+authorsRouter.put("/:authorId", (req, res) => {
+  const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath));
+
+  const index = authorsArray.findIndex(
+    (author) => author.id === req.params.authorId
+  );
+
+  const oldAuthor = authorsArray[index];
+
+  const updatedAuthor = { ...oldAuthor, ...req.body, updatedAt: new Date() };
+
+  authorsArray[index] = updatedAuthor;
+
+  fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray));
+
+  res.send(updatedAuthor);
 });
 
 export default authorsRouter;
